3e10dd0e2e81a2fc557e5f3dbeb382be80efa201
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-09-17 12:13:24 +0200

    Added Maven Modules. Closes #1.

--- pet-clinic-web/test/java/com/burntcity/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/test/java/com/burntcity/petclinic/PetClinicApplicationTests.java
@@ -1,8 +1,5 @@
 package com.burntcity.petclinic;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class PetClinicApplicationTests {
 
