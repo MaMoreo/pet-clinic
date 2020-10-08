a5b23834460601350913363e004d5be36c4b07a5
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-10-08 12:44:54 +0200

    Added Vet index and controller. Closes #6

---
+++ pet-clinic-web/src/main/java/com/burntcity/petclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package com.burntcity.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("vet")
+public class VetController {
+
+	@RequestMapping({ "", "/", "index", "index.html" })
+	public String index() {
+
+		return "vets/index.html";
+	}
+}
