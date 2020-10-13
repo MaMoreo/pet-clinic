dd41b7e0bb63a5f77798cc2cd06dd4e4e1b438e4
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-10-13 14:41:22 +0200

    Closes #22. Added Id auto generated

--- pet-clinic-data/src/main/java/com/burntcity/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/burntcity/petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,16 @@
 package com.burntcity.petclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+import com.burntcity.petclinic.model.BaseEntity;
 
-	protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+	protected Map<Long, T> map = new HashMap<>();
 
 	public Set<T> findAll() {
 		return new HashSet<>(map.values());
@@ -17,16 +20,30 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 
-	public T save(ID id, T object) {
-		map.put(id, object);
+	public T save(T object) {
+		if (object != null) {
+			if (object.getId() == null) {
+				object.setId(getNextId());
+			}
+			map.put(object.getId(), object);
+		}else {
+			throw new RuntimeException();
+		}
 		return object;
 	}
-	
+
 	public void deleteById(ID id) {
 		map.remove(id);
 	}
-	
+
 	public void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+
+	private Long getNextId() {
+		if(map.isEmpty()) {
+			return 1L;
+		}
+		return Collections.max(map.keySet()) + 1;
 	}
 }
--- pet-clinic-web/src/main/java/com/burntcity/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/burntcity/petclinic/bootstrap/DataLoader.java
@@ -23,31 +23,26 @@ public class DataLoader implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
 		owner1.setFirstName("Bruce");
 		owner1.setSecondName("Wayne");
 		ownerService.save(owner1);
 
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
 		owner2.setFirstName("Peter");
 		owner2.setSecondName("Parker");
 		ownerService.save(owner2);
 		
 		Owner owner3 = new Owner();
-		owner3.setId(3L);
 		owner3.setFirstName("Steve");
 		ownerService.save(owner3);
 		
 		System.out.println("Loaded Owners....");
 
 		Vet vet1 = new Vet();
-		vet1.setId(1L);
 		vet1.setFirstName("Sam");
 		vetService.save(vet1);
 
 		Vet vet2 = new Vet();
-		vet2.setId(2L);
 		vet2.setFirstName("Jessie");
 		vet2.setSecondName("James");
 		vetService.save(vet2);
