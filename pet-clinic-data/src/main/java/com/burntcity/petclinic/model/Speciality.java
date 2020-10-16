235af39abb5151fe6bfeee82ff2e2aeb09eed53d
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-10-16 15:22:44 +0200

    Added Specialty Entity, Enhanced Vet.Closes #27.

--- pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Speciality.java
@@ -1,13 +1,11 @@
 package com.burntcity.petclinic.model;
 
-import java.util.Set;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Vet extends Person {
-
-	private Set<Speciality> specialities;
+public class Speciality extends BaseEntity {
+	
+	private String description;
 }
