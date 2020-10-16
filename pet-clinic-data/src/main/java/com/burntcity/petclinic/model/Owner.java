57d8d0f84cbe4ad7897f826ece894a29bc9030f0
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-10-16 15:30:53 +0200

    Contact info Properties to Owner Closes #28

--- pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Owner.java
@@ -9,5 +9,9 @@ import lombok.Setter;
 @Setter
 public class Owner extends Person {
 	
+	// Contact
+	private String address;
+	private String city;
+	private String telephone;
 	private Set<Pet> pets;
 }
