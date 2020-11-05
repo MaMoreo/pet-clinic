dd8bf216bc4302c3ac2b595a68ee9f79417a3120
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-11-05 18:11:28 +0100

    Converted Vets to JPA Entities. Closes #37

--- pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Speciality.java
@@ -1,11 +1,18 @@
 package com.burntcity.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 	
+	//@ManyToMany(mappedBy = "specialities")  // this is NOT in the video
 	private String description;
 }
--- pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Vet.java
@@ -3,12 +3,24 @@ package com.burntcity.petclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
-
+	@ManyToMany(fetch = FetchType.EAGER) // by default, Many Relationships are lazy
+	@JoinTable(name="vet_specialties",  // the name of the table that will be created
+		joinColumns = @JoinColumn(name="vet_id"),  // the name of this column will be
+		inverseJoinColumns = @JoinColumn(name="speciality_id")) // the name of this column will be
 	private Set<Speciality> specialities = new HashSet<>();
 }
