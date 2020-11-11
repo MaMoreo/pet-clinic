8eb63924059eca31244715448c3fe9669b0b72f0
Miguel Moreo <miguelangel.moreo@gmail.com>
2020-11-11 15:19:50 +0100

    Added @Builder lombok

--- pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Speciality.java
@@ -3,11 +3,17 @@ package com.burntcity.petclinic.model;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialties")
 public class Speciality extends BaseEntity {
--- pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/burntcity/petclinic/model/Visit.java
@@ -1,17 +1,24 @@
 package com.burntcity.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity {
